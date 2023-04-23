3a9a61fbcc3543119a90c06871f11e46ab0c37cb
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-04-23 21:51:41 +0400

    1.7 update

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -2,7 +2,9 @@ import React from "react";
 
 class Feedback extends React.Component{
     render(){
-        return (<div>привет</div>)
+        return (<div className="feedback">
+            <span>Отзывы о нас</span>
+        </div>)
     }
 }
 export default Feedback
