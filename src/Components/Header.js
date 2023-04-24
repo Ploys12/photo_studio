b532f5af60859939aafb005f8e1f4762fe067713
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-04-24 22:23:56 +0400

    Добавлена навигация в главной странице

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,14 @@ class Header extends React.Component{
         return (<header>
             <div className="inner">
                 <img className="logo" src={this.props.src} alt="logo"></img>
-                
+                <div className="main_nav">
+                    <h2 onClick={() => {
+                    window.scrollTo( {top: 900, behavior: "smooth"} )
+                }}>Наши работы</h2>
+                    <h2 onClick={() => {
+                    window.scrollTo( {top: 1850, behavior: "smooth"} )
+                }}>Отзывы о нас</h2>
+                </div>
                 <ul className="nav">
                     <li onClick={() => {
                         this.setState({
