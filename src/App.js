e44f1221d0ba4024cc78182e91d64ddcfbc9d0b6
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-04-24 21:26:24 +0400

    Добавлены отзывы и стили к ним

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import Fourth from './img/fiveth.jpg'
 import Trust from './img/trust.jpg'
 import Our_Job from './Components/Our_Job';
 import Feedback from './Components/Feedback';
-
+import first_family from './img/first_family.png'
+import second_family from './img/second_family.jpg'
+import third_family from './img/third_family.jpg'
+import fourth_family from './img/fourth_family.jpg'
 import logo from './img/logo.png'
 
 
@@ -21,7 +24,8 @@ class App extends React.Component{
     return (<div className='wrapper'>
       <Header src={logo}/>
       <Our_Job first={First} second={Second} third={Third} fourth={Fourth} alt="photo"/>
-      <Feedback src={Trust} />
+      <Feedback src={Trust} first_family={first_family} alt="" second_family={second_family} third_family={third_family}
+       fourth_family={fourth_family}/>
       <Footer/>
     </div>)
   }
--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -14,6 +14,31 @@ class Feedback extends React.Component{
                 
                 
             </div>
+            <div className="feedback_div">
+                <div className="feedback_item">
+                    <div className="item first">
+                        <img src={this.props.first_family} alt={this.props.alt} className="family_image first"></img>
+                        <p className="family_text">Семья Горбуновых</p>
+                        <p className="description">Давно искали фотостудию для общего семейного альбома. Любимые моменты сделали нам альбом быстро и недорого! Рекомендую!</p>
+                    </div>
+                    <div className="item second">
+                    <img src={this.props.second_family} alt={this.props.alt} className="family_image second"></img>
+                    <p className="family_text">Семья Ивановых</p>
+                    <p className="description">Благодаря данной фотостудии наша семья смогла выступить на международном конкурсе красоты! Спасибо</p>
+                    </div>
+                    <div className="item third">
+                    <img src={this.props.third_family} alt={this.props.alt} className="family_image third"></img>
+                    <p className="family_text">Семья Ольгиных</p>
+                    <p className="description">Недавно с женой праздновали 10 годовщину свадьбы и решили сделать совместные фотографии с детьми. Теперь они укарашают наш дом:)</p>
+                    </div>
+                    <div className="item fourth">
+                    <img src={this.props.fourth_family} alt={this.props.alt} className="family_image fourth"></img>
+                    <p className="family_text">Семья Прошиных</p>
+                    <p className="description">Благодаря тому, что фотостудия располагается рядом с море, нам сделали фотографии на пляже. Получилось очень красиво!</p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>)
     }
 }
